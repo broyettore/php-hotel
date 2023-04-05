36cd2397bea04edeaf950bb3cc2c758c4b536d5b
broyettore <broyettore@gmail.com>
2023-04-05 16:48:41 +0200

    Project Scaffolding and basic styling

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Google font  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700;900&display=swap" rel="stylesheet">
+    <!-- Bootstrap  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!-- my css  -->
+    <link rel="stylesheet" href="assets/styles/style.css">
+    <title>Hotel Ivory</title>
+</head>
+<body>
+
+<div class="parallax">
+    <header>
+        <div class="container p-2 d-flex align-items-center justify-content-between">
+            <div class="logo">
+                <h1 class="d-flex align-items-center ">
+                    <a href=".">
+                    Hotel Ivory
+                    </a>
+                </h1>
+            </div>
+            <nav>
+                <ul class="d-flex">
+                    <li class="p-2 m-2">
+                        <a href="#">
+                            Services
+                        </a>
+                    </li>
+                    <li class="p-2 m-2">
+                        <a href="#">
+                            Gallery
+                        </a>
+                    </li>
+                    <li class="p-2 m-2">
+                        <a href="#">
+                            About Us
+                        </a>
+                    </li>
+                    <li class="py-2 my-2 ps-2 ms-2">
+                        <a href="#">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <main>
+        <section id="jumbo">
+        <div class="container ms-jumbo"></div>
+        </section>
+    </main>
+</div>
+
+
+
+
+
+
+    <!-- Bootstrap js  -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Php Hotel
+
+
+## Descrizione
+
+Partiamo da questo array di hotel. https://www.codepile.net/pile/OEWY7Q1G
+
+Stampare tutti i nostri hotel con tutti i dati disponibili.
+Prima stampare in pagina i dati, senza preoccuparvi dello stile.
+
+Dopo aggiungere Bootstrap e mostrare le informazioni con una tabella.
+
+Bonus:
+
+* Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
